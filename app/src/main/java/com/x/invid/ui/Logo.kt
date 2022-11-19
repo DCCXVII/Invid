b192c9d17b70e88dfdd5587ba1ef2242eb577342
elyass <elyass.inbox@gmail.com>
2022-11-19 12:51:02 -0800

    finished project

---
+++ app/src/main/java/com/x/invid/ui/Logo.kt
@@ -0,0 +1,13 @@
+package com.x.invid.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.x.invid.R
+
+class Logo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.logo)
+    }
+}
